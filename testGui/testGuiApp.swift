ba5e2c1aa587346715dd3c09ad46e9bf6aaf1048
jakubSz <jszymczak@student.agh.edu.pl>
2022-08-25 10:46:37 +0200

    Initial Commit

---
+++ testGui/testGuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGuiApp.swift
+//  testGui
+//
+//  Created by Jakub Szymczak on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct testGuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
